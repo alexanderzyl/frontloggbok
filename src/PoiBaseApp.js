65bbe7a01a0c486846c93552cded4e3c89029645
azyl <a.zyl@secureone.eu>
2024-09-16 10:30:53 +0200

    button Store Np

--- src/PoiBaseApp.js
+++ src/PoiBaseApp.js
@@ -5,7 +5,29 @@ import ImageCarousel  from "./ImageCarousel";
 import './PoiBaseApp.css';
 import PoiInfo from "./PoiInfo";
 import SortedPois from "./SortedPois";
+import axios from "axios";
 
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+const async_store_np = async (np_id) => {
+    const token = localStorage.getItem('token');
+    console.log('Token:', token);
+    if (!token || token === 'undefined') {
+        // Redirect to login if no token
+        window.location.href = '/';
+        return;
+    }
+    try {
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        }
+        const res = await axios.put(`${backendUrl}/user/store_np/${np_id}`, {}, { headers });
+        // window.location.href = '/user';
+    } catch (err) {
+        console.error('Failed to add np:', err);
+        window.location.href = '/';
+    }
+};
 const PoiBaseApp = () => {
     const [navPoints, setNavPoints] = useState([]);
     const [npInfo, setNpInfo] = useState({});
@@ -71,6 +93,10 @@ const PoiBaseApp = () => {
         }
     }
 
+    const handleStoreCityButtonClick = () => {
+        async_store_np(npInfo.id).then();
+    }
+
     const poiStateButtonName = (npDetailsState === "plan")? "Calculate Distances" : "Back to Overview";
 
     return (
@@ -78,6 +104,9 @@ const PoiBaseApp = () => {
             {mode === 'poisState' && npDetailsState && (
                 <div className={`additional-component ${npDetailsState}`}>
                     <AdditionalComponent />
+                    <button className={"poi-state-button"} onClick={handleStoreCityButtonClick}>
+                        Store City
+                    </button>
                     <button className={"poi-state-button"} onClick={handlePoiStateButtonClick}>
                         {poiStateButtonName}
                     </button>
