11b817326f000fb198ae1398e10ee93caae28968
azyl <a.zyl@secureone.eu>
2024-09-17 16:29:41 +0200

    check login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PoiBaseApp from "./PoiBaseApp";
 import Login from "./Login";
 import Poi from "./Poi";
 import User from "./User";
 
-const App = () => (
-    <Router>
-        <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/user" element={<User />} />
-            <Route path="/poi/:shortId" element={<Poi />} />
-            <Route path="/poibase" element={<PoiBaseApp />} />
-        </Routes>
-    </Router>
-);
+const App = () => {
+    useEffect(() => {
+        const isFirstLoad = sessionStorage.getItem('isAppLoaded');
+        if (!isFirstLoad) {
+            localStorage.clear();
+            sessionStorage.setItem('isAppLoaded', 'true');
+        }
+    }, []);
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/user" element={<User />} />
+                <Route path="/poi/:shortId" element={<Poi />} />
+                <Route path="/poibase" element={<PoiBaseApp />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
--- src/User.js
+++ src/User.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Tabs } from 'antd';
 import PoiTable from "./PoiTable";
 import GroupTable from "./GroupTable";
+import axios from "axios";
 
 const User = () => {
+    const backendUrl = process.env.REACT_APP_BACKEND_URL;
+    const [curUser, setCurUser] = useState({});
+
+    const getAuthHeaders = () => {
+        const token = localStorage.getItem('token');
+        if (!token || token === 'undefined') {
+            window.location.href = '/';
+            return {};
+        }
+        return {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        };
+    };
+
+    const fetchUser = async () => {
+        try {
+            const headers = getAuthHeaders();
+            const res = await axios.get(`${backendUrl}/user/me`, { headers });
+            setCurUser(res.data);
+        } catch (err) {
+            window.location.href = '/';
+        }
+    };
+
+    useEffect(() => {
+        fetchUser().then();
+    }, []);
+
     const items = [
         {
             key: 'groups',
